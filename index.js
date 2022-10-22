30dd686070f20f30a16362a7be3a86382132d2c2
VixiaID <me@vixia.xyz>
2022-10-22 13:42:25 +0700

    fix

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const { mediafire } = require('./lib/download/mediafire')
-const { spotify } = requiere('spotify')
-const { YtToMp3, YtToMp4 } = require('./lib/download/youtube')
-const { zippy } = require('./lib/download/zippyshare')
-
-module.exports.mediafire = mediafire
-module.exports.spotify = spotify
-module.exports.YtToMp3 = YtToMp3
-module.exports.YtToMp4 = YtToMp4
-module.exports.zippy = zippy
+"use strict";
+module.exports = require('./lib/download/index');
